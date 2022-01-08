834bf7d64c831b6a510cd9802b0556103dbf46fb
Antonio <ditacj78@gmail.com>
2022-01-08 11:24:12 +0100

    init project app

--- auth-app/src/app/app-routing.module.ts
+++ auth-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './features/login/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- auth-app/src/app/app.module.ts
+++ auth-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/login/login.component';
+import { LoginFormComponent } from './features/login/login-form/login-form.component';
+import { DashboardModule } from './features/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
