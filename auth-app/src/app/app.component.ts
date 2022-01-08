e92c02516c9d3445770e90bbf96e5b7d86b819e1
Antonio <ditacj78@gmail.com>
2022-01-08 13:22:57 +0100

    add new authMethods

--- auth-app/src/app/app.component.ts
+++ auth-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,24 @@ export class AppComponent {
     email: '',
     password: ''
   }
+
+  constructor(private authService: AuthService) {
+
+  }
   Submit() {
-    console.log(this.user)
+    console.log(this.user);
+    const {email, password} = this.user
+    this.authService.register(email, password).then(res => {
+      console.log('Register completed: ', res);
+    })
+  }
+
+  SubmitWithGoogle() {
+    console.log(this.user);
+    const {email, password} = this.user
+    this.authService.loginWithGoogle(email, password).then(res => {
+      console.log('Register completed with Google: ', res);
+    })
   }
 
 }
